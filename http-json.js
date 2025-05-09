3416a96ca86ae4ea090af7d748ed21c4e32f19a8
elena <elenapolishchukdev@gmail.com>
2025-05-09 10:59:07 +0300

    init

---
+++ http-json.js
@@ -0,0 +1,67 @@
+import http from "http";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    const url = new URL(req.url, `https://${req.headers.host}`);
+
+    if (req.method === "GET" && url.pathname === '/hello') {
+        res.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
+        const name = url.searchParams.get("name");
+        res.write(`Hello, ${name}!`)
+        res.end();
+        return;
+    }
+
+    if (req.method === "POST" && url.pathname === '/hello') {
+        let body = ''
+        req.on('data', (chunk) => {
+            body += chunk;
+        })
+        req.on('end', () => {
+            try {
+                const person = JSON.parse(body);
+                res.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
+                res.write(`Hello, ${person.firstName} ${person.lastName}!`)
+            } catch (e) {
+                res.writeHead(400, {"Content-Type": "text/plain; charset=utf-8"});
+                res.write(`Invalid JSON Response: ${e}`);
+            }
+            res.end();
+        })
+        return;
+    }
+
+    if (req.method === "POST" && url.pathname === '/feed') {
+        let body = ''
+        req.on('data', (chunk) => {
+            body += chunk;
+        })
+        req.on('end', () => {
+            try {
+                const person = JSON.parse(body);
+                res.writeHead(200, {"Content-Type": "application/json; charset=utf-8"});
+                const personFeed={
+                    fullName:`${person.firstName} ${person.lastName}`,
+                    foods:['Candies','Cookies', 'Cakes']
+                }
+                res.write(JSON.stringify(personFeed));
+            } catch (e) {
+                res.writeHead(400, {"Content-Type": "text/plain; charset=utf-8"});
+                res.write(`Invalid JSON Response: ${e}`);
+            }
+            res.end();
+        })
+        return;
+    }
+
+    res.writeHead(404, {"Content-Type": "text/plain"});
+    res.end("Not Found");
+})
+
+server.listen(port, () => {
+    console.log(`Listening on port ${port}. Press CTRL+C to finish.`);
+})
